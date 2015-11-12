737faa0393289081ebdf2c9158c0b1ddcdb173ca
kevinL974 <kevin.laoussing@etu.univ-st-etienne.fr>
2015-11-12 12:08:24 +0100

    [KLG] add enum RDFFormat

--- src/main/java/webSemLB/run/Run.java
+++ src/main/java/webSemLB/run/Run.java
@@ -1,10 +1,14 @@
 package webSemLB.run;
 
+import webSemLB.ClassExtractor.impl.ClassifierImpl;
+
 public class Run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ClassifierImpl classifier = new ClassifierImpl();
+		
+		classifier.getAllTypes("http://www.emse.fr/~zimmermann/Teaching/SemWeb/w3cstaff.html");
 	}
 
 }
